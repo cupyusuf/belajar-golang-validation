6d00650495db909edf8cf1d8dcf456be8deedc0b
Yusuf Supriadi <yusufdolenk2@gmail.com>
2025-03-11 10:28:41 +0700

    feat: add validasi struct

--- validation_test.go
+++ validation_test.go
@@ -55,3 +55,21 @@ func TestTagParameter(t *testing.T) {
 		fmt.Println(err.Error())
 	}
 }
+
+func TestStruct(t *testing.T) {
+	type LoginRequest struct {
+		Email    string `validate:"required,email"`
+		Password string `validate:"required,min=5"`
+	}
+
+	validate := validator.New()
+	loginRequest := LoginRequest{
+		Email:    "yusuf@gmail.com",
+		Password: "yusuf10",
+	}
+
+	err := validate.Struct(loginRequest)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
